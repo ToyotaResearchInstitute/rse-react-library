c3b13ff03473acd252370b8e3733c6f306290cf3
mike puzon <michael.puzon@tri.global>
2024-05-14 13:56:32 -0700

    Export Feedback button

--- lib/index.ts
+++ lib/index.ts
@@ -3,5 +3,6 @@ import './global.css';
 export { default as tailwindConfig } from './tailwind.config';
 export { default as TRIApp, type TRIAppConfig, useTRIAppContext } from './tri-app';
 export { Highlight, Highlighter } from './components/highlighter';
+export { FeedbackButton } from './layout/FeedbackButton';
 export { useUserAuth } from './amplify/use-user-auth';
 export { Select } from './components/select';
